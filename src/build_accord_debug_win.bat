aced00a3b06ea70d4882ad8142452caafce3ecad
Adam Noel <adamjgnoel@gmail.com>
2016-02-18 13:03:03 -0500

    Updated build scripts to create bin if it doesn't exist

--- src/build_accord_debug_win.bat
+++ src/build_accord_debug_win.bat
@@ -1,1 +1,3 @@
+@echo off
+if not exist "..\bin\" mkdir "..\bin"
 gcc accord.c mtwist.c randistrs.c region.c subvolume.c meso.c micro_molecule.c chem_rxn.c actor.c base.c observations.c timer_accord.c mol_release.c actor_data.c file_io.c cJSON.c -std=c99 -g -o "..\bin\accord_win_debug.exe"

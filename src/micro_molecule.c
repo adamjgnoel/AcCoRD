0b484b7e02113a62b1d248dbf7b377b95f133b0b
Adam Noel <adamjgnoel@gmail.com>
2016-03-02 13:33:35 -0500

    Hard-coded absorption
    Wrote variation that makes any type 0 molecule entering a new region to
    be turned into type 1. Done to improve accuracy of absorbing receiver
    simulations before implementation of reactive surfaces is complete.

--- src/micro_molecule.c
+++ src/micro_molecule.c
@@ -115,8 +115,9 @@ void diffuseMolecules3D(const short NUM_REGIONS,
 	{
 		for(curType = 0; curType < NUM_MOL_TYPES; curType++)
 		{
-			if(isListMol3DEmpty(&p_list[curRegion][curType]))
-				continue; // No need to validate an empty list of molecules
+			if(isListMol3DEmpty(&p_list[curRegion][curType])
+				|| sigma[curRegion][curType] == 0.)
+				continue; // No need to validate an empty list of molecules or ones that can't move
 			
 			curNode = p_list[curRegion][curType];
 			
@@ -133,7 +134,8 @@ void diffuseMolecules3D(const short NUM_REGIONS,
 	{
 		for(curType = 0; curType < NUM_MOL_TYPES; curType++)
 		{
-			if(isListMol3DEmpty(&p_list[curRegion][curType]))
+			if(isListMol3DEmpty(&p_list[curRegion][curType])
+				|| sigma[curRegion][curType] == 0.)
 				continue; // No need to validate an empty list of molecules
 			
 			curNode = p_list[curRegion][curType];
@@ -175,14 +177,26 @@ void diffuseMolecules3D(const short NUM_REGIONS,
 							// Molecule is now in a different region
 							if(regionArray[newRegion].spec.bMicro)
 							{ // New region is microscopic. Move to appropriate list
-								if(!addMolecule3D(&p_list[newRegion][curType],
+								if(curType == 0)
+								{
+									if(!addMolecule3D(&p_list[newRegion][1],
+									newPoint[0], newPoint[1], newPoint[2]))
+									{
+										fprintf(stderr, "ERROR: Memory allocation to move molecule between microscopic regions %u and %u.\n", curRegion, newRegion);
+										exit(EXIT_FAILURE);
+									}
+									// Indicate that molecule doesn't need to be moved again
+									p_list[newRegion][1]->item.bNeedUpdate = false;
+								} else if(!addMolecule3D(&p_list[newRegion][curType],
 									newPoint[0], newPoint[1], newPoint[2]))
 								{
 									fprintf(stderr, "ERROR: Memory allocation to move molecule between microscopic regions %u and %u.\n", curRegion, newRegion);
 									exit(EXIT_FAILURE);
+								} else
+								{
+									// Indicate that molecule doesn't need to be moved again
+									p_list[newRegion][curType]->item.bNeedUpdate = false;
 								}
-								// Indicate that molecule doesn't need to be moved again
-								p_list[newRegion][curType]->item.bNeedUpdate = false;
 							} else
 							{ // New region is mesoscopic. Find nearest subvolume to new point
 								newSub = findNearestSub3D(newRegion, regionArray,
@@ -233,7 +247,7 @@ void diffuseMolecules3D(const short NUM_REGIONS,
 			curNodeR = p_listRecent[curRegion][curType];
 			curMol = 0;
 			while(curNodeR != NULL)
-			{
+			{				
 				oldPoint[0] = curNodeR->item.x;
 				oldPoint[1] = curNodeR->item.y;
 				oldPoint[2] = curNodeR->item.z;
